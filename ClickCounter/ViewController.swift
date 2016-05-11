20f502dfcad0096ef81d40cc4a4349582c45f833
Raj <vivinraj@gmail.com>
2016-05-11 07:07:35 +0530

    Click Counter Full

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 0
+   // let countString : String = count.stringValue
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = "\(count)"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +32,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func Click(sender: AnyObject) {
+        count = count + 1
+        countLabel.text = "\(count)"
+        
+        
+    }
 
 }
 
